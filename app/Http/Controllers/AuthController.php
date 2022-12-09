065512c1f202d31a80c2a936eb73ae82a382d1dd
Saiful kamil <saifulkamil18@gmail.com>
2022-12-09 17:35:30 +0700

    ubah respon regis dan login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Auth\Events\Logout;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
@@ -25,8 +26,9 @@ class AuthController extends Controller
         ];
         $validator = Validator::make($req->all(), $rules);
         if ($validator->fails()) {
-            // return response()->json($validator->errors(), 400);
-            return $response = ['message' => 'email sudah didaftarkan'];
+            // $response = ['message' => 'Incorrect email or password'];
+            return $response = ['value' => '2'];
+            //response()->json($validator->errors(), 400);
         }
         //create new user in users table
         $user = User::create([
@@ -40,8 +42,8 @@ class AuthController extends Controller
             'password' => Hash::make($req->password)
         ]);
         $token = $user->createToken('Personal Access Token')->plainTextToken;
-        $response = ['user' => $user, 'token' => $token];
-        return response()->json($response, 200);
+        return $response = ['value' => '1', 'user' => $user, 'token' => $token];
+        //return response()->json($response, 200);
     }
 
     public function login(Request $req)
@@ -57,11 +59,13 @@ class AuthController extends Controller
         // if user email found and password is correct
         if ($user && Hash::check($req->password, $user->password)) {
             $token = $user->createToken('Personal Access Token')->plainTextToken;
-            $response = ['user' => $user, 'token' => $token];
-            return response()->json($response, 200);
+            return $response = ['value' => '1', 'user' => $user, 'token' => $token];
+            //$response = ['user' => $user, 'token' => $token];
+            //return response()->json($response, 200);
         }
-        $response = ['message' => 'Incorrect email or password'];
-        return response()->json($response, 400);
+        // $response = ['message' => 'Incorrect email or password'];
+        // return response()->json($response, 400);
+        return $response = ['message' => 'Incorrect email or password', 'value' => '2'];
     }
 
     public function me(Request $req)
@@ -75,7 +79,8 @@ class AuthController extends Controller
 
     public function logout(Request $req)
     {
-        $req->user()->currentAccessToken()->delete();
+        $req->user()->currentAccessToken('Logout acces token')->delete();
+
         $this->response['message'] = 'succes';
 
         return response()->json($this->response, 200);
